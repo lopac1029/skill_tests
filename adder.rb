a9999a5f0ea44aff2edef4bbf2191dc95664faba
Aaron Yarhouse <lopac1029@gmail.com>
2012-06-04 17:45:17 -0400

    Made vars easier to read

--- adder.rb
+++ adder.rb
@@ -1,27 +1,46 @@
+puts
 puts 'Please enter a number of 20 or greater.'
-n = gets.chomp.to_i
-until n > 19
-	puts 'You entered ' + n.to_s + ', which is not equal to or greater than 20. Please choose again correctly.' 
-	n = gets.chomp.to_i
+number = gets.chomp.to_i
+
+until number > 19
+	puts
+	puts 'You entered ' + number.to_s + ', which is not equal to or greater than 20.'
+	puts 'Please choose again correctly.' 
+	number = gets.chomp.to_i
 end
-if n >= 20
+
+if number >= 20
+	puts
 	puts 'Please chose an option of 1, 2, or 3.'
 end
-op = gets.chomp.to_i
-until op.between?(1, 3)
-	puts 'You did not choose a correct option. Please choose again correctly.'
-	op = gets.chomp.to_i
+
+option = gets.chomp.to_i
+
+until option.between?(1, 3)
+	puts
+	puts 'You did not choose a correct option'
+	puts 'Please choose again correctly.'
+	option = gets.chomp.to_i
 end
-if op == 1
-	i=((n*n)-(op*op)+n+op)/2;
-	puts 'The sum of all numbers between 1 and ' + n.to_s + ' is ' + i.to_s + '.'
-elsif op == 2
-	puts 'The odd numbers from 1 to ' + n.to_s + ' are '
+
+if option == 1
+	int=((number*number)-(option*option)+number+option)/2;
+	puts
+	puts 'The sum of all numbers between 1 and ' + number.to_s + ' is ' + int.to_s + '.'
+	puts
+elsif option == 2
+	puts
+	puts 'The odd numbers from 1 to ' + number.to_s + ' are: '
 	x = 1
-	while x <= n
-		puts x
+	array = []
+	while x <= number
+		array << x
 		x=x+2
 	end
-elsif op == 3
-	puts 'Thanks for entering number ' + n.to_s + '! Option ' + op.to_s + ' does nothing.'
+	puts array.join(', ')
+	puts
+elsif option == 3
+	puts
+	puts 'Thanks for entering number ' + number.to_s + '! Option ' + option.to_s + ' does nothing.'
+	puts
 end
