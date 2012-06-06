4b88fc620bbfa28c2e1d0cfab289d748362d0426
Aaron Yarhouse <lopac1029@gmail.com>
2012-06-06 13:20:59 -0400

    Made recusive question

--- main.rb
+++ main.rb
@@ -1,25 +1,31 @@
 require '../skill_tests/lib/random_60.rb'
 require '../skill_tests/lib/adder.rb'
+#require '../skill_tests/lib/gas.rb'
 
-puts "Which program do you want to start?
-	\nEnter:
+def ask question
+	puts question
+	request = gets.downcase.chomp
+
+	if request == 'adder'
+		run = Adder.new
+		run.get_numbers
+	elsif request == 'random'
+		run = Random_60.new
+		run.view
+	elsif request == 'gas'
+		puts 'Gas is working properly'#run = Gas.new
+		#run.view
+	elsif request == 'exit'
+		exit
+	else
+		puts "That is not a valid answer"
+		ask question
+	end
+end
+
+ask "\nWhich program do you want to start?\nEnter:
 	Adder		choose a number and an option to modify it by
 	Random		random numbers between -10 and 10, and their percentages
+	Gas		calculate your rental cars gas consumption
 		or
 	Exit		exit the program"
-request = gets.downcase.chomp
-
-until request == 'adder'||request == 'random'||request == 'exit'
-	puts 'That is not a valid answer'
-	request = gets.chomp
-end
-if request == 'adder'
-	run = Adder.new
-	run.get_numbers
-elsif request == 'random'
-	run = Random_60.new
-	run.view
-elsif request == 'exit'
-	exit
-end
-	
